0eb15dfe48ca436db4e81521f5963c121b8b2af9
kensuke suzuki <35k.0101@gmail.com>
2020-11-17 07:54:19 +0900

    add:filter

--- script.js
+++ script.js
@@ -13,6 +13,23 @@ async function getPosts() {
   return data;
 };
 
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+
+  posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+}
+
 // Show posts in DOM
 async function showPosts() {
   const posts = await getPosts();
@@ -55,3 +72,5 @@ window.addEventListener('scroll', () => {
     showLoading()
   }
 })
+
+filter.addEventListener('input', filterPosts);
